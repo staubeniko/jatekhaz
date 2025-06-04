530f9881eb3f9f5826b4ca525b0dcae3b468c26a
staubeniko <staub.eniko@gmail.com>
2025-06-04 16:22:53 +0200

    Update x.js

--- x.js
+++ x.js
@@ -1,11 +1,12 @@
-  function zoomImage(imgElement) {
+function zoomImage(imgElement) {
     const overlay = document.getElementById("overlay");
     const zoomedImg = document.getElementById("zoomed-img");
     zoomedImg.src = imgElement.src;
     overlay.style.display = "flex";
-  }
+    document.body.classList.add("overlay-active");
+}
 
-  function closeZoom() {
+function closeZoom() {
     document.getElementById("overlay").style.display = "none";
-  }
-
+    document.body.classList.remove("overlay-active");
+}
